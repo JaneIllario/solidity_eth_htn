c16ee268fd3630ee8d65f3e9ad62110da7dfa30a
JaneIllario <illariojane@gmail.com>
2018-09-15 04:13:39 -0400

    fixed fcn call

---
+++ pet-shop-tutorial/migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+/* 2_deploy_contracts.js.
+The numbered prefix indicates what order the migration file should be run.
+The suffix is extra optional descriptions.
+*/
+
+// Sets the varible `Adoption` to whatever Contract is in `Adoption`.
+// Here, the contract name is used rather than the file name, as one file can have multiple contracts.
+
+var Staff_checkin = artifacts.require("Staff_checkin");
+
+// All migrations in Truffle need to export via `module.exports`.
+// More details here: https://truffleframework.com/docs/truffle/getting-started/running-migrations
+module.exports = function(deployer) {
+  deployer.deploy(Staff_checkin)
+}
